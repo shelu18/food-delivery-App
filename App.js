d12f15c93e0e25902370972e4a105ffca977533b
shailendra parihar <shailendraparihar210156@acropolis.in>
2024-04-03 06:59:57 +0530

    Add cart component and update styles

--- App.js
+++ App.js
@@ -1,29 +1,51 @@
 //creating a food delivery app
-import React from 'react';
+import React from "react";
 import ReactDOM from "react-dom";
-import './style.css';
+import "./style.css";
 
-const Header =() =>{
-    return (
-    <div className ="header">
-    <div className ="logo-container">
-    <img  className="logo" src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-delivery-by-simplepixelsl-brandcrowd.png" alt="logo"/>
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://s3.amazonaws.com/cdn.designcrowd.com/blog/39-Food-Delivery-Logos-That-Will-Leave-You-Hungry-For-More/food-delivery-by-simplepixelsl-brandcrowd.png"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
-    <div className="nav-items">
-    <ul>
-    <li>Home</li>
-    <li>Menu</li>
-    <li>Contact</li>
-    </ul> 
+  );
+};
+
+const Cart = () => {
+  return <div className="cart"></div>;
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div ClassName="search"> Search</div>
+      <div className="res-container">
+        <Cart />
+      </div>
     </div>
+  );
+};
+
+const Applayout = () => {
+  return (
+    <div>
+      <Header />
+      <Body />
     </div>
-    );
-}
-const Applayout =() =>{
-    return (
-    <Header/>
-    );
-    
-}
-const root =ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Applayout/>);
+  );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Applayout />);
